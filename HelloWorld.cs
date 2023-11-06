9fbbfe3a9cdd82924a928b217941e73d1dd6a893
lox2034 <150064911+lox2034@users.noreply.github.com>
2023-11-06 20:43:54 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("error sans only likes chocolate");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
